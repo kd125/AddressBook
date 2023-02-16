f7e0068c2058825d14396f48476b726172b3f7c8
Kd125 <kashdangar1250@gmail.com>
2023-02-15 20:55:24 -0600

    end of class commit

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-let arrayOfUsers = [];
-
 window.onload = function () {
-  displayUsers();
+  fetchUser();
 };
 
 const fetchUser = () => {
   fetch("https://randomuser.me/api/?results=10")
     .then((res) => res.json())
-    .then((posts) => (arrayOfUsers = posts));
+    .then((data) => {
+      displayUsers(data.results);
+    });
 };
 
 const consoleUser = () => {
   console.log(arrayOfUsers);
 };
 
-const displayUsers = () => {
+const displayUsers = (arrayOfUsers) => {
   const allUsers = document.getElementById("displayUserList");
   arrayOfUsers.map((post, index) => {
     const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = post.picture.large;
     const text = document.createTextNode(
-      `#${index}, gender: ${post.gender}: ${post.name} `
+      `#${index}, ${post.picture.large} Gender: ${post.gender}: Name: ${post.name.first} ${post.name.last}, DOB: ${post.dob.date}, Address: ${post.location.street.number} ${post.location.street.name}, ${post.location.city}`
     );
     li.appendChild(text);
+    li.appendChild(img);
     allUsers.appendChild(li);
   });
 };
