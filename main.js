778a88660d036c47a0475851117189defbaa315e
Kd125 <kashdangar1250@gmail.com>
2023-02-15 20:11:37 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-let userList = [];
+let arrayOfUsers = [];
+
+window.onload = function () {
+  displayUsers();
+};
 
 const fetchUser = () => {
-  fetch("https://randomuser.me/api/")
+  fetch("https://randomuser.me/api/?results=10")
     .then((res) => res.json())
-    .then((user) => (userList = user));
+    .then((posts) => (arrayOfUsers = posts));
+};
+
+const consoleUser = () => {
+  console.log(arrayOfUsers);
+};
+
+const displayUsers = () => {
+  const allUsers = document.getElementById("displayUserList");
+  arrayOfUsers.map((post, index) => {
+    const li = document.createElement("li");
+    const text = document.createTextNode(
+      `#${index}, gender: ${post.gender}: ${post.name} `
+    );
+    li.appendChild(text);
+    allUsers.appendChild(li);
+  });
 };
